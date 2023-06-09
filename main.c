4af0b5f0eae80f95f6a4cee8f086ab616188582b
linuxu <linuxu@ParallelC23-7.academic.management.afeka.local>
2023-06-09 15:02:13 +0300

    init3

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ Initially the array of size 4*PART is known for the process 0.
 It sends the half of the array to the process 1.
 Both processes start to increment members of thier members by 1 - partially with OpenMP, partially with CUDA
 The results is send from the process 1 to the process 0, which perform the test to verify that the integration worked properly
+hey
 */
  
 
